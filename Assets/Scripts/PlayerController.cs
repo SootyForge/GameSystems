85d871026d5bee98d85b7544ba00808871444670
SootyForge <42379545+SootyForge@users.noreply.github.com>
2018-08-31 12:16:33 +1000

    Week 6

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,20 +9,23 @@ using UnityEngine;
  */
 
 // CTRL + K + D (Cleans code)
-public class W3_Player : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+    public bool rotateToMainCamera = false;
+    public Transform weapon;
+
     public float moveSpeed = 5f;
     public float jumpHeight = 10f;
     public Rigidbody rigid;
     public float rayDistance = 1f; // How many units the ray is drawn below the player.
+    public LayerMask ignoreLayers;
     
     private bool isGrounded = false;
 
 // Implement this OnDrawGizmosSelected if you want to draw gizmos only if the object is selected
-    private void OnDrawGizmos()
+    void OnDrawGizmos()
     {
         Ray groundRay = new Ray(transform.position, Vector3.down);
-        Gizmos.color = Color.red;
         Gizmos.DrawLine(groundRay.origin, groundRay.origin + groundRay.direction * rayDistance);
     }
 
@@ -31,7 +34,7 @@ public class W3_Player : MonoBehaviour
         Ray groundRay = new Ray(transform.position, Vector3.down);
         RaycastHit hit;
         // Casts a line beneath the player
-        if(Physics.Raycast(groundRay, out hit, rayDistance))
+        if(Physics.Raycast(groundRay, out hit, rayDistance, ~ignoreLayers))
         {
             // is grounded
             return true;
@@ -45,10 +48,22 @@ public class W3_Player : MonoBehaviour
     {
         float inputH = Input.GetAxis("Horizontal") * moveSpeed;
         float inputV = Input.GetAxis("Vertical") * moveSpeed;
+
         Vector3 moveDir = new Vector3(inputH, 0f, inputV);
+
+        // Get the euler angles of Camera
+        Vector3 camEuler = Camera.main.transform.eulerAngles;
+
+        // Is the contorller rotating to the camera?
+        if (rotateToMainCamera)
+        {
+            // Calculate the new move direction by only taking into account the Y Axis
+            moveDir = Quaternion.AngleAxis(camEuler.y, Vector3.up) * moveDir;
+        }
+
         Vector3 force = new Vector3(moveDir.x, rigid.velocity.y, moveDir.z);
 
-        if (Input.GetButton("Jump") && IsGrounded())
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
             force.y = jumpHeight;
         }
@@ -56,11 +71,17 @@ public class W3_Player : MonoBehaviour
         rigid.velocity = force;
 
         // If the user pressed a key (moveDir has values in it other than 0)
-        if(moveDir.magnitude > 0)
+        /*if(moveDir.magnitude > 0)
         {
             // Rotate the player to that moveDir
             transform.rotation = Quaternion.LookRotation(moveDir);
-        }
+        }*/
 
+        // TESTING!
+
+        Quaternion playerRotation = Quaternion.AngleAxis(camEuler.y, Vector3.up);
+        Quaternion weaponRotation = Quaternion.AngleAxis(camEuler.x, Vector3.right);
+        weapon.localRotation = weaponRotation;
+        transform.rotation = playerRotation;
     }
 }
